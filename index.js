c34f771b759a635b722de4ad428ba58a06a616fa
Jose Salazar <solisjose466@gmail.com>
2022-06-09 15:54:35 -0700

    toggle between sections implemented, need to document it

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const menuItems = document.querySelectorAll(".menu-items");
+const sections = document.querySelectorAll(".sections")
 function handleClick(target, elements){
+    const targetClasses = target.className; 
+    let targetName = targetClasses.replace("menu-items", "");
+
+    console.log(`clicked ${targetName}`);
+
     elements.forEach((element) => {
-        console.log(element);
+        let elementClasses = element.className; 
+        let elementName = elementClasses.replace("sections", "");
+
+        if(targetName === elementName){
+            element.hidden = false;
+        }
+        else{
+            element.hidden = true;
+        }
+
+        console.log(`iterating over ${elementName}`);
     });
 }
 
 menuItems.forEach(menuItem => {
     menuItem.addEventListener("click", ()=>{
-        handleClick(menuItem, menuItems);
+        handleClick(menuItem, sections);
     });
 });
